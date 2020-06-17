63fe7d64d92560b7d6d1f1d7c80f884777470ef5
loc <locn049@gmail.com>
2020-06-17 23:39:10 +0900

    new data

---
+++ app/src/main/java/com/example/myworker/MainTho.kt
@@ -0,0 +1,11 @@
+package com.example.myworker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainTho : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_tho)
+    }
+}
